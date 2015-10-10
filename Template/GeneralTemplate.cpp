658f273b189c5d95dc2489f2a70ad0f3238193dc
Charlespartina <tangziyi001@gmail.com>
2015-10-10 15:43:03 -0400

    Update

---
+++ Template/GeneralTemplate.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <cstdio>
+#include <string>
+#include <cstring>
+#include <sstream>
+#include <cmath>
+#include <algorithm>
+#include <vector>
+#include <set>
+#include <map>
+#include <stack>
+#include <queue>
+using namespace std;
+typedef long long ll;
+typedef vector<int> vi;
+typedef pair<int,int> pii;
+typedef vector<ii> vpi;
+typedef vector<vpi> vvpi;
+
+int main(){
+
+
+
+
+
+	return 0;
+}
---
+++ Template/UnionFind.cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+#include <cstdio>
+#include <string>
+#include <cstring>
+#include <sstream>
+#include <cmath>
+#include <algorithm>
+#include <vector>
+#include <set>
+#include <map>
+#include <stack>
+#include <queue>
+using namespace std;
+typedef vector<int> vi;
+typedef pair<int,int> pii;
+typedef vector<ii> vpi;
+typedef vector<vpi> vvpi;
+
+// Union Find
+class UnionFind{
+private:
+	vi p,rank;
+public:
+	UnionFind (int N){
+		rank.assign(N,0);
+		p.assign(N,0);
+		for (int i = 0; i < N; i++){
+			p[i] = i;
+		}
+	}
+	int findRep(int i){
+		if (p[i] == i){
+			return i;
+		}
+		else{
+			p[i] = findRep(p[i]); // Update parent to the root
+			return p[i];
+		}
+	}
+	bool isSameSet(i, j){
+		int x = findRep(i);
+		int y = findRep(j);
+		return x == y;
+	}
+	void unionSet(int i, int j){
+		if (!isSameSet(i,j)){
+			if (rank[i] > rank[j]){
+				p[j] = i;
+			}
+			else if(rank[i] < rank[j]){
+				p[i] = j;
+			}
+			else{
+				p[i] = j;
+				rank[j]++;
+			}
+		}
+	}
+
+};
+int main(){
+
+
+
+
+
+	return 0;
+}
