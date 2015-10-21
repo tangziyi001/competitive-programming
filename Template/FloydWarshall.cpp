a113cfe9d6041c66839161239bf1c17e4bcdd112
Charlespartina <tangziyi001@gmail.com>
2015-10-20 23:46:24 -0400

    Update

--- Template/FloydWarshall.cpp
+++ Template/FloydWarshall.cpp
@@ -1,5 +1,5 @@
 /*
- *	
+ *	Floyd Warshall’s algorithm
  *	Created by Ziyi Tang
  *
  */
@@ -26,12 +26,31 @@ typedef pair<int,int> pi;
 typedef vector<pi> vpi;
 typedef vector<vpi> vvpi;
 const int dir[4][2] = {{1,0},{-1,0},{0,1},{0,-1}};
+#define MAXN 100
 
+int v,e;
+ll fw[MAXN][MAXN];
 int main(){
-
-
-
-
-
+	// No Self Loop 
+	for (int i = 0; i < v; i++){
+		for (int j = 0; j < v; j++){
+			fw[i][j] = i == j ? 0 : INFL;
+		}
+	}
+	// Fill in the Adj Matrix
+	for (int i = 0; i < e; i++){
+		ll a,b,c;
+		cin >> a >> b >> c;
+		fw[a][b] = c;
+	}
+	for (int k = 0; k < v; k++){
+		for (int i = 0; i < v; i++){
+			for (int j = 0; j < v; j++){
+				if (fw[i][k] == INFL || fw[k][j] == INFL)
+					continue;
+				fw[i][j] = min(fw[i][j], fw[i][k] + fw[k][j]);
+			}
+		}
+	}
 	return 0;
 }
