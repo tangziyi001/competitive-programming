4fe8298178c4db8c341c2b52d94efd76972162f4
Charlespartina <tangziyi001@gmail.com>
2015-10-22 17:19:39 -0400

    Update

--- DP/TSL/GettingInLine.cpp
+++ DP/TSL/GettingInLine.cpp
@@ -1,7 +1,7 @@
 /*
  *	UVA 216
  *	Created by Ziyi Tang
- *	Bitmask in TSP problem
+ *	Bitmask in TSP problem, track the next step, multiple starting point
  */
 
 #include <iostream>
@@ -29,33 +29,37 @@ const int dir[4][2] = {{1,0},{-1,0},{0,1},{0,-1}};
 
 double mat[10][10];
 double dp[10][1024];
-vector<pair<int, int> > all;
+int pt[10][1024];
+vpi all;
 int num;
 
 double tsp(int pos, int path){
 	if (path == (1 << num) - 1){
-		return dp[pos][path] = mat[pos][0];
+		return 0;
 	}
 	if (dp[pos][path] != -1){
-		//cout << pos << " " << path << " binary "<< dp[pos][path] << endl;
 		return dp[pos][path];
 	}
 	else{
 		double minp = INF;
+		int minnxt;
 		for (int i = 0; i < num; i++){
 			if ((path & (1 << i)) == 0){
-				cout << path << "path" << endl;
 				double tmp = mat[pos][i]+tsp(i, path | (1 << i));
-				if (minp > tmp)
+				if (minp > tmp){
 					minp = tmp;
+					minnxt = i;
+				}
 			}
 		}
-		return dp[pos][path] = minp;
+		pt[pos][path] = minnxt;
+	 	return dp[pos][path] = minp;
 	}
 }
 int main(){
-	
+	int count = 0;
 	while (scanf("%d", &num) && num != 0){
+		count++;
 		for (int i = 0; i < 10; i++){
 			for (int j = 0; j < 10; j++){
 				mat[i][j] = 0;
@@ -67,6 +71,8 @@ int main(){
 			}
 		}
 		all.clear();
+		
+
 		for (int t = 0; t < num; t++){
 			int x,y;
 			scanf("%d %d", &x, &y);
@@ -80,15 +86,43 @@ int main(){
 			}
 			
 		}
-		int path = 1;
-		double re = tsp(0, path);
-		// for (int i = 0 ; i < num; i++){
-		// 	for (int j = 0; j < num; j++){
-		// 		cout << mat[i][j] << " ";
-		// 	}
-		// 	cout << endl;
-		// }
-		printf("%lf\n", re);
+		
+		int start = 0;
+		double re = INF; 
+		int fp[10][1024];
+		for (int i = 0; i < num; i++){
+			int path = 1 << i;
+			double tmp = tsp(i, path);
+			if (re > tmp){
+				re = tmp;
+				for (int i = 0; i < 10; i++){
+					for (int j = 0; j < 1024; j++){
+						fp[i][j] = pt[i][j];
+					}
+				}
+				start = i;
+			}
+			for (int i = 0; i < 10; i++){
+				for (int j = 0; j < 1024; j++){
+					dp[i][j] = -1;
+				}
+			}
+			memset(pt,0,sizeof(pt));
+		}
+
+		printf("**********************************************************\n");
+		printf("Network #%d\n",count);
+		int bp = 1 << start;
+		int next = fp[start][bp];
+		printf("Cable requirement to connect (%d,%d) to (%d,%d) is %.2lf feet.\n", all[start].first,all[start].second, all[next].first, all[next].second, mat[start][next]);
+		for (int i = 1; i < num-1; i++){
+			bp |= (1<<next);
+			int ori = next;
+			next = fp[next][bp];
+			printf("Cable requirement to connect (%d,%d) to (%d,%d) is %.2lf feet.\n", all[ori].first,all[ori].second, all[next].first, all[next].second, mat[ori][next]);
+		}
+		printf("Number of feet of cable required is %.2lf.\n", re);
+		
 	}	
 
 
