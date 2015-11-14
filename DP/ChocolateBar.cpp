553e127fa3755ff5d722ad4c831d7b67cf49d3f4
Charlespartina <tangziyi001@gmail.com>
2015-11-14 10:05:09 -0500

    Update

--- DP/ChocolateBar.cpp
+++ DP/ChocolateBar.cpp
@@ -33,9 +33,39 @@ const int dir[4][2] = {{-1,0},{0,1},{1,0},{0,-1}};
 #define FILL(x,v) memset(x,v,sizeof(x))
 
 
+int dp[35][35][55];
+int rec(int i, int j, int k){
+	if(k == 0 || i*j == k)
+		return 0;
+	if(k > i*j)
+		return INF;
+	if(dp[i][j][k] != -1)
+		return dp[i][j][k];
+	int minp = INF;
+	REP(tk,0,k+1){
+		int rk = k - tk;
+		REP(ti,1,i){
+			minp = min(minp, j*j + rec(ti,j,tk) + rec(i-ti,j,rk));
+		}
+		REP(tj,1,j){
+			minp = min(minp, i*i + rec(tj,i,tk) + rec(j-tj,i,rk));
+		}
+	}
+	return dp[i][j][k] = minp;
 
+}
 int main(){
-	
+	FILL(dp,-1);
+	REP(i,1,31) REP(j,1,31) REP(k,1,51){
+		rec(i,j,50);
+	}
+	int cas;
+	int n,m,k;
+	cin >> cas;
+	REP(i,0,cas){
+		cin >> n >> m >> k;
+		cout << rec(n,m,k) << endl;
+	}
 
 	return 0;
 }
